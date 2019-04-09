5ca35ceb0df480f5bfc57199a153f2edfe641973
Omphalus Kua <haluskua.one@gmail.com>
2019-04-09 20:44:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 // write your code below!
-function happyHolidays () {
+function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo (name) {
+ function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
 
-function holidayCountdown(holiday, days){
-  return `It\'s ${days} days until ${holiday}!`
+ function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
 }
 
+ function holidayCountdown(holiday, days){
+  return `It\'s ${days} days until ${holiday}!`
+}// write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+ function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
+}
+
+ function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
+}
+
+ function holidayCountdown(holiday, days){
+  return `It\'s ${days} days until ${holiday}!`
+}
