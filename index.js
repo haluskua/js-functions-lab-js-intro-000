1ee5fe4beca08aaf1c9c1006f4f2174b5e3df2a4
Omphalus Kua <haluskua.one@gmail.com>
2019-04-09 21:07:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function happyHolidays() {
  function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
-happyHolidayTo('Dude');
+happyHolidayTo('you');
 
  function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
